bb0ab827a525823bc77ba69f214393ec96f1e615
AMITKURIL98 <amitkuril@opticalarc.com>
2024-01-18 16:56:33 +0530

    Created 5 Components
    pushed by Amit Kuril

--- OMS/src/App.jsx
+++ OMS/src/App.jsx
@@ -1,13 +1,23 @@
 import { Routes, Route } from "react-router-dom";
 import ErrorPage from "./Components/ErrorPage";
-import Home from "./Components/Home";
+import LoginPage from "./Components/LoginPage";
+import AddExpence from "./Components/ClientReport/AddExpence";
+import ExpensePage from "./Components/ClientReport/ExpensePage";
+import ClientList from "./Components/ClientReport/ClientList";
+import AddClient from "./Components/ClientReport/AddClientPaymentAcc";
+import ProjectReport from "./Components/ClientReport/ProjectReport";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<AddExpence />} />
+        <Route path="expenses" element={<ExpensePage />} />
+        <Route path="clientlist" element={<ClientList />} />
+        <Route path="addclient" element={<AddClient />} />
+        <Route path="projectreport" element={<ProjectReport />} />
         <Route path="*" element={<ErrorPage />} />
+        <Route path="login" element={<LoginPage />} />
       </Routes>
     </>
   );
